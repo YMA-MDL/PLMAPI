0dd8ee28e5ead7f35bff96aab3e9102879ab076d
YMA-MDL <yoann.maingon@mydatalinx.com>
2015-11-23 19:30:53 +0000

    returns first BOM level when adding a child part

--- PLMAPI/api/controllers/PartController.js
+++ PLMAPI/api/controllers/PartController.js
@@ -18,18 +18,19 @@ module.exports = {
 	    if (properties.parentPart === properties.partReference) return res.badRequest();
 	    	// if it creates a circular reference 
 	    	// TODO
-	    
-	    
-	    // if everything ok => create the relationship and add it to the partBomRelationships collection
-	    /* global PartBOMRelationship */
-	    PartBOMRelationship.create(properties).exec(function (err, partBomRelationship) {
-	    	if (err) return res.badRequest(err);
-	    	Part.findOne(properties['parentPart']).exec(function(err,part){
-	    		if (err) return res.badRequest(err);
-	    		res.ok(part);
-	    	});
+	    	// circular reference on a single level
+	    PartBOMRelationship.count({parentPart: properties['partReference'],partReference:properties['parentPart']}).exec(function(err, found){
+	    	if (found >0)  return res.badRequest("found circular reference on level 0");	
+		    // if everything ok => create the relationship and add it to the partBomRelationships collection
+		    /* global PartBOMRelationship */
+		    PartBOMRelationship.create(properties).exec(function (err, partBomRelationship) {
+		    	if (err) return res.badRequest(err);
+		    	Part.findOne(properties['parentPart']).populate('partBOMRelationships').exec(function(err,part){
+		    		if (err) return res.badRequest(err);
+		    		res.ok(part);
+		    	});
+    		});
 	    });
-	    
 	},
 	
 	updatePartBOMRelationship : function(req,res){
